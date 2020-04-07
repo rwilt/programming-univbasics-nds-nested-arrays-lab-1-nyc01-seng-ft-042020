7562a5a6049e2b4474ac3e310fc45d896ef5ebc2
rosie w <rosie.wilt@gmail.com>
2020-04-07 15:50:37 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -37,12 +37,17 @@ def sorted_matrix
   sort_con = CONVENTIONAL_PRODUCE.sort
   org_con = ORGANIC_PRODUCE.sort
   while counter < sort_con.length do
-    new_array.push(sort_con[counter])
-    new_array.push(org_con[counter])
+    new_array.push([sort_con[counter]])
+    new_array.push([org_con[counter]])
     counter += 1
   end
   new_array
 end
+
+
+rspec ./spec/nested_array_spec.rb:8 # When a learning to build nested arrays, 
+the sorted_matrix method returns a nested array made from two constants
+with sorted elements
 
 def matrix_lookup(matrix, row, column)
   matrix[row][column]
