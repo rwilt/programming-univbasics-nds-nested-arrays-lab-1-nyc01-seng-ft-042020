e996168256782883033d0da67a3a469b4ccd578e
rosie w <rosie.wilt@gmail.com>
2020-04-07 15:52:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,16 +34,15 @@ end
 def sorted_matrix
   counter = 0
   new_array = []
-  sort_con = CONVENTIONAL_PRODUCE.sort
-  org_con = ORGANIC_PRODUCE.sort
+  sort_con = [CONVENTIONAL_PRODUCE.sort]
+  org_con = [ORGANIC_PRODUCE.sort]
   while counter < sort_con.length do
-    new_array.push([sort_con[counter]])
-    new_array.push([org_con[counter]])
+    new_array.push(sort_con[counter])
+    new_array.push(org_con[counter])
     counter += 1
   end
   new_array
 end
-
 
 def matrix_lookup(matrix, row, column)
   matrix[row][column]
